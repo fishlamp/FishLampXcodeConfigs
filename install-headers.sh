44ef7ecd5a49393f3dffbd0b00d2542ac8c20d25
Mike Fullerton <mike@greentongue.com>
2013-08-10 15:53:25 -0700

    update script

--- install-headers.sh
+++ install-headers.sh
@@ -10,13 +10,13 @@ BUILT_PRODUCTS_DIR="$1"
 PRODUCT_NAME="$2"
 PROJECT_DIR="$3"
 
-echo "$PRODUCT_NAME"
-echo "$PROJECT_DIR"
-echo "$BUILT_PRODUCTS_DIR"
+# echo "$PRODUCT_NAME"
+# echo "$PROJECT_DIR"
+# echo "$BUILT_PRODUCTS_DIR"
 
 dest="$BUILT_PRODUCTS_DIR/$PRODUCT_NAME"
 
-echo "$dest"
+# echo "$dest"
 
 if [ -d "$dest" ]; then
     rm -R "$dest"
@@ -24,14 +24,17 @@ fi
 
 mkdir -p "$dest"
 
-cd "$PROJECT_DIR"
-files=`find . -name "*.h"`
+function copyHeadersInDir() {
+    cd "$1"
+    files=`find . -name "*.h"`
 
-echo "files: $files"
+    for file in $files; do
+        filename=`basename "$file"`
+        destpath="$dest/$filename"
+        echo "copied $file to $destpath"
+        cp "$file" "$destpath"
+    done
+}
 
-for file in $files; do
-    filename=`basename "$file"`
-    destpath="$dest/$filename"
-    echo "$file to $destpath"
-    cp "$file" "$destpath"
-done
+copyHeadersInDir "$PROJECT_DIR/Classes"
+copyHeadersInDir "$PROJECT_DIR/../Classes"
