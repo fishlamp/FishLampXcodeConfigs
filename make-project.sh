a02e262dd9c5dac5d8a76c1bc74addbc2a639b59
Mike Fullerton <mike@greentongue.com>
2013-09-07 12:10:23 -0700

    rename script

---
+++ make-project.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function usage() {
+    echo "bash new-project.sh \"../myProject\" \"Lib\" \"MyApp\""
+}
+
+if [ "$1" == "" ]; then
+    echo "first param should be path to your git project"
+    usage
+    exit 1;
+fi
+
+if [ "$2" == "" ]; then 
+    echo "second param should be name of project subfolder"
+    usage
+    exit 1;
+fi
+
+if [ "$3" == "" ]; then
+    echo "third param should be project name"
+    exit 1;
+fi
+
+git submodule update --init --recursive
+
+py Piecemeal-Scripts/NewProjectFromTemplate.py ProjectTemplate "$destPath" "$projectName" "$subFolderName"
--- new-project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git submodule update --init --recursive
-
-py Piecemeal-Scripts/NewProjectFromTemplate.py ProjectTemplate "$destPath" "$projectName" "$subFolderName"
