e5e07845ff5872d8a5f63ce8cb4080cc3a0d3668
Mike Fullerton <mike@Mikes-MacPro.local>
2013-11-16 14:29:18 -0800

    remove file

--- ProjectSettings/PiecemealCompilerFlagOverrides.h
+++
@@ -1,7 +0,0 @@
-
-// some of the warning we don't want are turned on by -Wall. This turns them off.
-// simply include this file before anything else in your precompiled header file.
-
-
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wunused-function"
